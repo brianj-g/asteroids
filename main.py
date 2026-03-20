9493cfcab78ce6279c17913a5eb105cc713c23ea
brianj-g <brian.gunther@outlook.com>
2026-03-19 22:32:25 -0700

    fixed delta time

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-        dt = game_clock.tick() / 1000
+        dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
